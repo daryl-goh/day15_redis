dd9c4da2a46795b199cd712be9204a2cac6a195a
daryl-goh <goh.daryl@gmail.com>
2022-08-01 20:40:57 +0800

    make it better

--- src/main/java/vttp2022/ssf/day15_redis/controllers/GreetingsController.java
+++ src/main/java/vttp2022/ssf/day15_redis/controllers/GreetingsController.java
@@ -1,41 +1,39 @@
 package vttp2022.ssf.day15_redis.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(path={"/", ""})
 public class GreetingsController {
 
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    @Qualifier("redislab")
+    private RedisTemplate<String, String> redisTemplate;
 
     @GetMapping
     public String getGreetings(Model model) {
-        ValueOperations<String, Object> ops = redisTemplate.opsForValue();
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
         Object greetings = ops.get("greetings");
         model.addAttribute("hello", greetings.toString());
         return "index";
     }
 
     @PostMapping
-    public String postGreetings(
-        @RequestParam String newGreetings,
-        Model model) {
-        ValueOperations<String, Object> ops = redisTemplate.opsForValue();
-        ops.set("greetings", newGreetings);
-        model.addAttribute("greetings", newGreetings);
+    public String postGreetings(@RequestBody MultiValueMap<String, String> form, Model model) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String text = form.getFirst("text");
+        ops.set("greetings", text);
+        model.addAttribute("hello", text);
         return "index";
-
-        }
-
-    
-    
+    }
 }
